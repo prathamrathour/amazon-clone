46f43074f76be884202bbc8a365ebec2bde3999d
Pratham Rathour <prathamrathour2018@gmail.com>
2021-07-09 21:40:03 +0530

    creating react app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZreFmE51p0Ka856ON8GaADVAerGaA1fY",
+  authDomain: "clone-656e3.firebaseapp.com",
+  projectId: "clone-656e3",
+  storageBucket: "clone-656e3.appspot.com",
+  messagingSenderId: "127266185476",
+  appId: "1:127266185476:web:9abc183c910df8ea11b051",
+  measurementId: "G-Z7RJTTYBPQ",
+};
